9d49111ffc00d5a53da1a5cd6fbbb87822f336cc
onifernando1 <onifernando1@gmail.com>
2023-12-11 15:38:54 +0000

    function to get song names in array from playlist link

--- spotify2mp3/routes/index.js
+++ spotify2mp3/routes/index.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var router = express.Router();
 require("dotenv").config();
+const axios = require("axios");
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
@@ -11,7 +12,23 @@ router.get("/spotify", function (req, res, next) {
   res.send("Spotify");
 });
 
-router.post("/spotifyrequest", function (req, res, next) {
-  res.send("Request");
+router.get("/spotifyrequest", async function (req, res, next) {
+  let config = {
+    headers: { Authorization: `Bearer ${process.env.ACCESS_TOKEN}` },
+  };
+
+  const playlist_songs = await axios.get(
+    "https://api.spotify.com/v1/playlists/25b8hPQhuJ09M4BHBxstX8/tracks?limit=100",
+    config
+  );
+
+  let songsArray = [];
+
+  playlist_songs.data.items.forEach((item) =>
+    songsArray.push(item.track.name + " " + item.track.artists[0].name)
+  );
+
+  res.send(songsArray);
 });
+
 module.exports = router;
