1f8b539202a142cb60c9927e3801208066c1af79
onifernando1 <onifernando1@gmail.com>
2023-12-11 14:39:54 +0000

    skeleton route to post to spotify api

--- spotify2mp3/routes/index.js
+++ spotify2mp3/routes/index.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var router = express.Router();
+require("dotenv").config();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
@@ -10,4 +11,7 @@ router.get("/spotify", function (req, res, next) {
   res.send("Spotify");
 });
 
+router.post("/spotifyrequest", function (req, res, next) {
+  res.send("Request");
+});
 module.exports = router;
